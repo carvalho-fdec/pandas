f08b116850d0c2473229259c466f13f354d3147a
Fernando <carvalho.fdec@gmail.com>
2022-06-05 12:53:40 -0300

    Create iatradedobem.py

---
+++ ia_tradedobem/iatradedobem.py
@@ -0,0 +1,5 @@
+"""
+Projeto usando Inteligência Artificial para escolher a melhor ação para comprar de uma lista pré-definida.
+IA project to choose the best stock to buy on a list of selected stocks.
+"""
+
