4e62d0cc366c03dd0bc7b08d01ccf3d94aef8ac5
Fernando <carvalho.fdec@gmail.com>
2022-06-05 13:00:24 -0300

    Update iatradedobem.py

--- ia_tradedobem/iatradedobem.py
+++ ia_tradedobem/iatradedobem.py
@@ -1,5 +1,6 @@
 """
 Projeto usando Inteligência Artificial para escolher a melhor ação para comprar de uma lista pré-definida.
 IA project to choose the best stock to buy on a list of selected stocks.
+teste de alteração
 """
 
