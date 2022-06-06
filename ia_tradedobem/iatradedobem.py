153d32628a439ccf8185836a3d30ec2eaed6b235
Fernando <carvalho.fdec@gmail.com>
2022-06-06 20:20:21 -0300

    variacoes semanais calculadas
    fazer o algoritmo que define a compra

--- ia_tradedobem/iatradedobem.py
+++ ia_tradedobem/iatradedobem.py
@@ -11,30 +11,38 @@ IA project to choose the best stock to buy on a list of selected stocks.
 
 import pandas as pd
 import pandas_datareader.data as web
-from datetime import date
+from datetime import date, timedelta
+import matplotlib.pyplot as plt
 
 hoje = date.today()
 
 lista_de_acoes = ["ABEV3.SA", "ARZZ3.SA", "B3SA3.SA", "BBDC3.SA", "BBSE3.SA", "EGIE3.SA", "ENBR3.SA", "EZTC3.SA", "GRND3.SA", "ITSA3.SA", "ITUB3.SA", "LEVE3.SA", "LREN3.SA", "MDIA3.SA", "ODPV3.SA", "PSSA3.SA", "RADL3.SA", "SLCE3.SA", "TOTS3.SA", "VIVT3.SA", "VULC3.SA", "WEGE3.SA"]
 
-df = web.DataReader("ABEV3.SA", "yahoo", hoje - pd.DateOffset(months=3), hoje)
-df = df.iloc[::-1] # inverter o dataframe para que o primeiro dia seja o mais recente
+for acao in lista_de_acoes:
+    df = web.DataReader(acao, "yahoo", hoje - pd.DateOffset(months=3), hoje)
+    df = df.iloc[::-1] # inverter o dataframe para que o primeiro dia seja o mais recente
+    # df.to_excel("dados.xlsx", sheet_name=acao)
 
-# criar um arquivo excell com os dados do df
-df.to_excel("dados2.xlsx")
+    lista_medias = []
+    for i in range(0, 90, 5):   # calcular a média até 90 dias para trás
+        media = df.loc[hoje-timedelta(i):hoje-timedelta(i+5), "Adj Close"].mean()
+        lista_medias.append(media)
 
-# calcular a média da coluna 'Adj Close' por semana
-cont_aux = 0
-soma = 0
-cont_semana = 1
-for i in df["Adj Close"]:
-    print(i)
-    if cont_aux == 5:
-        media_semana = soma / 5
-        print("Semana " + str(cont_semana) + ": " + str(media_semana))
-        cont_semana += 1
-        soma = 0
-        cont_aux = 0
-    soma += i
-    cont_aux += 1
+    media = 0
+    lista_variacao = []
+    for media_anterior in lista_medias:
+        if media == 0:
+            media = media_anterior
+        else:
+            variacao = (media - media_anterior) / media_anterior * 100
+            # formatar a variacao para float com 5 casas decimais
+            variacao = float("{:.5f}".format(variacao))
+            lista_variacao.append(variacao)
+            media = media_anterior
+    
+    print(f"lista de medias: {lista_medias}")
+    print(f"lista de variacoes: {lista_variacao}")
 
+    plt.plot(lista_variacao)
+    plt.plot(lista_medias)
+    plt.show()
